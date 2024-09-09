d19e888d0b091111dac9fa54707f9eea44fef174
Rayhan Aurelia Pramana Rijal <rayhanits2023@gmail.com>
2024-09-10 06:58:07 +0700

    Update Abstraction.java

--- Abstraction.java
+++ Abstraction.java
@@ -3,10 +3,10 @@ public class Abstraction {
     public static void main(String[] args) {
         // Creating objects from non-abstract subclasses
         HeroIntel hero1 = new HeroIntel("Rayhan");
-        hero1.display();  // Outputs: Aku adalah Otong
+        hero1.display();  
 
         HeroAgility hero2 = new HeroAgility("Mario");
-        hero2.display();  // Outputs: Aku adalah Mario
+        hero2.display();  
 
         // Creating an object from the abstract class is not allowed
         // Hero hero3 = new Hero("Mario"); // This line is commented out because it would cause an error
